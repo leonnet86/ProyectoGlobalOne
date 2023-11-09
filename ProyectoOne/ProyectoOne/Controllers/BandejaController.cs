85ff45fab9d984455a83daee005c56bcac8d907d
luke_eagen@hotmail.com <luke_eagen@hotmail.com>
2023-11-09 17:21:37 -0500

    mod proy

---
+++ ProyectoOne/ProyectoOne/Controllers/BandejaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoOne.Controllers
+{
+    public class BandejaController : Controller
+    {
+        // GET: Bandeja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
